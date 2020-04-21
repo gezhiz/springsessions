7c4979154239f00437d221b35f5d39221064d92a
gezhizheng <gezhizheng@58.com>
2020-04-21 23:17:16 +0800

    依赖查找

--- ioccontaineroverview/src/main/java/com/worthto/ioc/overview/DependencyLookUpDemo.java
+++ ioccontaineroverview/src/main/java/com/worthto/ioc/overview/DependencyLookUpDemo.java
@@ -2,9 +2,11 @@ package com.worthto.ioc.overview;
 
 import com.worthto.definition.bean.Person;
 import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.ObjectFactory;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 /**
+ * 通过名称查找
  * @author gezz
  * @description
  * @date 2020/4/21.
@@ -13,8 +15,24 @@ public class DependencyLookUpDemo {
 
     public static void main(String[] args) {
         BeanFactory beanFactory = new ClassPathXmlApplicationContext("classpath:META-INF/dependency-look-context.xml");
-        Person person = beanFactory.getBean(Person.class);
+        lookUpRealTime(beanFactory);
+        lookUpLazy(beanFactory);
+    }
+
+    /**
+     * 实时查找
+     */
+    private static void lookUpRealTime(BeanFactory beanFactory) {
+        Person person = (Person) beanFactory.getBean("person");
         System.out.println(person);
+    }
+
+    /**
+     * 延时查找
+     */
+    private static void lookUpLazy(BeanFactory beanFactory) {
+        ObjectFactory person = (ObjectFactory) beanFactory.getBean("personFactory");
+        System.out.println(person.getObject());
     }
 
 }
