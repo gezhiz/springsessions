f9f714ef3bbd48d8fab812baaf355f4264e0589d
gezhizheng <gezhizheng@58.com>
2020-04-21 21:18:03 +0800

    Initial commit

---
+++ definition/src/main/java/com/worthto/definition/bean/Person.java
@@ -0,0 +1,27 @@
+package com.worthto.definition.bean;
+
+/**
+ * @author gezz
+ * @description
+ * @date 2020/4/21.
+ */
+public class Person {
+    private String name;
+    private Integer age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+}
